fbdaa2bdad59d16a81d5db708d7f6bb0c67d14a9
Vasily Tsvetkov <vasily.tsvetkov@roguewave.com>
2019-03-13 11:48:31 +0200

    merge QRebelPublisher and QRebelStepPerformer

--- src/main/java/org/zeroturnaround/jenkins/plugin/qrebel/QRebelPublisher.java
+++ src/main/java/org/zeroturnaround/jenkins/plugin/qrebel/QRebelPublisher.java
@@ -9,13 +9,22 @@ package org.zeroturnaround.jenkins.plugin.qrebel;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.io.PrintStream;
+import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
+import org.jenkinsci.lib.envinject.EnvInjectException;
+import org.jenkinsci.plugins.envinjectapi.util.EnvVarsResolver;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.zeroturnaround.jenkins.plugin.qrebel.rest.BuildClassifier;
+import org.zeroturnaround.jenkins.plugin.qrebel.rest.Issues;
+import org.zeroturnaround.jenkins.plugin.qrebel.rest.QRebelRestApi;
+import org.zeroturnaround.jenkins.plugin.qrebel.rest.QRebelRestApiClient;
 
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractProject;
+import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.BuildStepDescriptor;
@@ -27,8 +36,8 @@ import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
 /**
- * Post-build step that keeps configuration. It can be persisted by XStream, so all the non-serializable fields are
- * kept in QRebelStepPerformer.
+ * A Jenkins build will be marked as failed if there are some issues detected by QRebel
+ * The issues are obtained via HTTP requests to the QRebel server. Some of them can be ignored depending on the plugin configuration.
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -49,11 +58,6 @@ public class QRebelPublisher extends Recorder implements SimpleBuildStep {
   final long exceptionFail;
   final long threshold;
 
-  @Override
-  public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws IOException {
-    QRebelStepPerformer logic = QRebelStepPerformer.make(this, run, listener);
-    logic.perform();
-  }
 
   @Symbol(PLUGIN_SHORT_NAME)
   @Extension
@@ -68,5 +72,94 @@ public class QRebelPublisher extends Recorder implements SimpleBuildStep {
     String getDisplayName() {
       return Messages.QRebelBuilder_DescriptorImpl_DisplayName();
     }
+  }
+
+  @Override
+  public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws IOException {
+    PrintStream logger = listener.getLogger();
+    Fields fields = resolveFields(run);
+    QRebelRestApi restApi = QRebelRestApiClient.create(fields.serverUrl);
+
+    logger.println("AppName: " + fields.appName);
+    logger.println("Baseline Build: " + fields.baselineBuild);
+    logger.println("Baseline Version: " + fields.baselineVersion);
+    logger.println("Target Build: " + fields.targetBuild);
+    logger.println("Target Version: " + fields.targetVersion);
+
+    Issues qRData;
+    if (StringUtils.isNotEmpty(fields.baselineBuild)) {
+      restApi.setDefaultBaseline(fields.apiKey, fields.appName, new BuildClassifier(fields.baselineBuild, fields.baselineVersion));
+      qRData = restApi.getIssuesVsBaseline(fields.apiKey, fields.appName, fields.targetBuild, fields.targetVersion, fields.durationFail, fields.ioFail, fields.exceptionFail, PluginVersion.get());
+    }
+    else {
+      qRData = restApi.getIssuesVsThreshold(fields.apiKey, fields.appName, fields.targetBuild, fields.targetVersion, fields.durationFail, fields.ioFail, fields.exceptionFail, PluginVersion.get());
+    }
+    IssuesStats stats = new IssuesStats(qRData);
+
+    boolean failBuild = qRData.issuesCount.DURATION > fields.durationFail
+        || qRData.issuesCount.IO > fields.ioFail
+        || qRData.issuesCount.EXCEPTIONS > fields.exceptionFail
+        || stats.isThresholdProvidedAndExceeded(fields.threshold);
+
+    if (failBuild) {
+      run.setResult(Result.FAILURE);
+      String initialDescription = run.getDescription();
+      run.setDescription(getFailureDescription(qRData, fields, initialDescription, stats.getSlowestDuration()));
+      logger.println("Performance regression have been found in the current build. Failing build.");
+
+      logger.println(String.format("Slow Requests: %d%n" +
+              " Excessive IO: %d %n" +
+              " Exceptions: %d  %n" +
+              " SLA global limit (ms): %d ms | slowest endpoint time(ms): %d ms",
+          qRData.issuesCount.DURATION, qRData.issuesCount.IO, qRData.issuesCount.EXCEPTIONS, fields.threshold, stats.getSlowestDuration()));
+
+      logger.println("For more details check your <a href=\"" + qRData.appViewUrl + "/\">dashboard</a>");
+    }
+  }
+
+  private Fields resolveFields(Run<?, ?> run) {
+    return Fields.builder()
+        .apiKey(resolveEnvVarFromRun(apiKey, run))
+        .appName(resolveEnvVarFromRun(appName, run))
+        .baselineBuild(resolveEnvVarFromRun(baselineBuild, run))
+        .baselineVersion(resolveEnvVarFromRun(baselineVersion, run))
+        .targetBuild(resolveEnvVarFromRun(targetBuild, run))
+        .targetVersion(resolveEnvVarFromRun(targetVersion, run))
+        .serverUrl(resolveEnvVarFromRun(serverUrl, run))
+        .durationFail(durationFail)
+        .exceptionFail(exceptionFail)
+        .ioFail(ioFail)
+        .threshold(threshold)
+        .build();
+  }
+
+  // resolve fields, NotNull
+  private static String resolveEnvVarFromRun(String value, Run<?, ?> run) {
+    try {
+      String resolved = EnvVarsResolver.resolveEnvVars(run, value);
+      return resolved == null ? "" : resolved;
+    }
+    catch (EnvInjectException e) {
+      throw new IllegalStateException("Unable to get Env Variable " + value, e);
+    }
+  }
+
+  // describe failure reason
+  private static String getFailureDescription(Issues qRData, Fields fields, String buildDescription, long slowestDuration) {
+    StringBuilder descriptionBuilder = new StringBuilder();
+    if (StringUtils.isNotEmpty(buildDescription)) {
+      descriptionBuilder.append(buildDescription);
+    }
+    descriptionBuilder.append(String.format("Failing build due to performance regressions found in %s compared to build %s version %s. <br/>" +
+            "Slow Requests: %d <br/>" +
+            "Excessive IO: %d <br/>" +
+            "Exceptions: %d <br/>" +
+            "SLA global limit (ms): %d ms | slowest endpoint time(ms): %d ms <br/>" +
+            "For full report check your <a href= %s >dashboard</a>.<br/>",
+        qRData.appName, fields.baselineBuild, fields.baselineVersion, qRData.issuesCount.DURATION,
+        qRData.issuesCount.IO, qRData.issuesCount.EXCEPTIONS,
+        fields.threshold, slowestDuration, qRData.appViewUrl));
+
+    return descriptionBuilder.toString();
   }
 }
